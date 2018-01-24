fdcaed675aa1b547e595be83400abc864aa43c59
Juan Carlos Campos <rapidclimate@gmail.com>
2018-01-24 14:00:21 -0600

    CORREGIDO ICONOS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { ClarityModule } from "@clr/angular";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { InicioComponent } from './inicio/inicio.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
