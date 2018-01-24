59dc8e3342349b2590dd332494cd58acc60624df
Juan Carlos Campos <rapidclimate@gmail.com>
2018-01-24 12:10:08 -0600

    jajaja

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {InicioComponent} from './inicio/inicio.component'
+import {DashboardComponent} from './dashboard/dashboard.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', redirectTo:'inicio',pathMatch:'full',},
+  {path:'inicio',component:InicioComponent},
+  {path:'dashboard', component:DashboardComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
